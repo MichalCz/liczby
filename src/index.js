847a2ff3e9395e78e3bcbd7048cbd3add789a421
Budleigh Salterton <budleigh.salterton@gmail.com>
2020-12-23 22:50:13 +0100

    Use to scramjet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const mongo = require('mongodb').MongoClient;
+const {DataStream} = require('scramjet');
 
 const url = 'mongodb://127.0.0.1:27017';
 const dbname = 'baza';
@@ -26,39 +27,42 @@ mongo.connect(url, {useUnifiedTopology: true}, (error, client) => {
         numbers: value
     });
 
-    const addOne = (value) => {
-        db.insertOne(list(value));
-    }
-
-
-    dropCollection()
+    const addOne = async (value) => db.insertOne(list(value));
+    
+    try {
+        await dropCollection();
+    } catch(e) { /* nuttin */ }
     // count()
     // findLast();
+    
+    DataStream.from(function*() {
+        for (let a = 1; a <= 44; ++a) {
 
-    for (let a = 1; a <= 44; ++a) {
+            for (let b = 2; b <= 45; ++b) {
+                if (b <= a) continue;
 
-        for (let b = 2; b <= 45; ++b) {
-            if (b <= a) continue;
+                for (let c = 3; c <= 46; ++c) {
+                    if (c <= b) continue;
 
-            for (let c = 3; c <= 46; ++c) {
-                if (c <= b) continue;
+                    for (let d = 4; d <= 47; ++d) {
+                        if (d <= c) continue;
 
-                for (let d = 4; d <= 47; ++d) {
-                    if (d <= c) continue;
+                        for (let e = 5; e <= 48; ++e) {
+                            if (e <= d) continue;
 
-                    for (let e = 5; e <= 48; ++e) {
-                        if (e <= d) continue;
+                            for (let f = 6; f <= 49; ++f) {
 
-                        for (let f = 6; f <= 49; ++f) {
-                            
-                            if (f <= e) continue;
-                            addOne([a, b, c, d, e, f]);
-                        } // loop f
-                    } // loop e
-                } // loop d
-            } // loop c
-        } // loop b
-    } // loop a
+                                if (f <= e) continue;
+                                yield [a, b, c, d, e, f];
+                            } // loop f
+                        } // loop e
+                    } // loop d
+                } // loop c
+            } // loop b
+        } // loop a
+    })
+        .do(addOne)
+        .consume();
 
     console.log('koniec')
 })
